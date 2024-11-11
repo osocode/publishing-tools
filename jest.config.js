7f0a05da5cfb8e1fc142ca38bf6c167204c9264e
Chris Burton <28830614+osocode@users.noreply.github.com>
2024-11-10 21:05:50 -0600

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  modulePathIgnorePatterns: ['/dist/']
+}
